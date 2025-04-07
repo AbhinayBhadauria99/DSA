f8fb3f3de15ea233682924c5ed1f50327b1e3573
CyborgScribe <113773196+AbhinayBhadauria99@users.noreply.github.com>
2025-04-07 18:23:49 +0530

    Time: 3 ms (26.51%), Space: 22.6 MB (16.99%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        int n= nums.size();
-        long long res=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                   res=max(res,1LL * (nums[i] - nums[j]) * 1LL * nums[k] );
-                }
-            }
+        int n = nums.size();
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+
+        for (int i = 1; i < n; i++) {  //storing max possible element at left of each element 
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1]);
         }
-        return res>0? res:0;
+        for(int i = n-2; i >= 0; i--) {   //storing max possible element at right of each element 
+            rightMax[i] = max(rightMax[i+1], nums[i+1]);
+        }
+
+        long long result = 0;
+        for (int j = 1; j < n - 1; j++) {
+            result = max(result, (long long)(leftMax[j] - nums[j]) * rightMax[j]);  //now just select elements from leftmax and rightmax so that our equation may give max result.
+        }
+        return result;
     }
 };
