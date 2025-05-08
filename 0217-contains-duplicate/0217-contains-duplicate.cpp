dc7b6d0a2133e51e825d91801af4bcd5ad24d29f
CyborgScribe <113773196+AbhinayBhadauria99@users.noreply.github.com>
2025-05-08 17:49:56 +0530

    Time: 27 ms (76.88%), Space: 71 MB (75.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1) return false;
+        for (int i=1;i<nums.size();i++){
+            if((nums[i-1]^nums[i])==0) return true;
+        }      
+        return false;
+    }
+};
