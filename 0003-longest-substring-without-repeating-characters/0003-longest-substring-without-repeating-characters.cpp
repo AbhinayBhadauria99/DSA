80d16450b7d8a04bb0d0667b14a148729d4adbbc
CyborgScribe <113773196+AbhinayBhadauria99@users.noreply.github.com>
2025-06-14 23:30:17 +0530

    Time: 7 ms (68.22%), Space: 11.9 MB (48.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int start=0;
-        int end=0;
         int count =0;
         int maxcount=0;
         unordered_map<int,int> mpp;
@@ -18,7 +17,6 @@ public:
                 start++;
             }
             count++;
-            end++;
             mpp[it]++;
             maxcount=max(maxcount,count);
         }
