599a118010e31472aeda2dfca42f93b3eca5b79e
CyborgScribe <113773196+AbhinayBhadauria99@users.noreply.github.com>
2025-09-02 16:45:52 +0530

    Time: 0 ms (100%), Space: 21.8 MB (44.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -8,21 +8,21 @@ public:
         while(i<intervals.size()){
             int temp1=intervals[i][0];
             int temp2=intervals[i][1];
-            if(first>temp2){
+            if(first>temp2){//interval aage hai abhi range me nahi
                 ans.push_back({temp1,temp2});
                 i++;
             }
-            else if(temp1>second){
-                break;
+            else if(temp1>second){ //intreval peeche reh gya ab tak merge ho chuka hoga
+                break; //yaha se break kar do loop ko
             }
-            else{
+            else{ //merge karte raho interval ko
                 first=min(first,temp1);
                 second=max(second,temp2);
                 i++;
             }
         }
-        ans.push_back({first,second});
-        while(i<intervals.size()){
+        ans.push_back({first,second});  //interval daal do
+        while(i<intervals.size()){ //break kiya tha to ab interval ke baad baaki bache hue daal do
             ans.push_back({intervals[i][0],intervals[i][1]});
             i++;
         }
